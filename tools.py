151e5ed287e88296a30fb0a26f4bc9bc67113f71
Gabriel Pelouze <gabriel@pelouze.net>
2018-03-21 10:54:49 +0100

    add gauss2d, convolve_with_filter, and unsharp_mask to tools

--- tools.py
+++ tools.py
@@ -66,3 +66,79 @@ def roll_2d(a, shift_i=None, shift_j=None):
     a = np.roll(a, shift_i, axis=0)
     a = np.roll(a, shift_j, axis=1)
     return a
+
+def gauss2d(nx, ny, sx, sy, normalized=True):
+    ''' Returns an array containing a 2D gaussian at its centre.
+
+    Parameters
+    ==========
+    nx, ny : int
+        The x and y size of the array.
+    sx, sy : float
+        The standard deviation of the gaussian along the x and y axes.
+    normalized : bool (default: True)
+        If True, generate a gaussian of integral 1. (*Warning*: this does not
+            necessarily imply that arr.sum() == 1, unless nx and ny are large
+            compared to sx and sy, respectively.)
+        If False, the peak of the gaussian is set to 1.
+
+    Returns
+    =======
+    arr : ndarray, shape (ny, nx)
+        An array containing the gaussian data
+    '''
+    x, y = np.meshgrid(np.arange(nx), np.arange(ny))
+    arr = np.exp( - (x-nx/2)**2 / (2*sx**2) - (y-ny/2)**2 / (2*sy**2) )
+    if normalized:
+        arr /= 2 * np.pi * sx * sy
+    return arr
+
+def convolve_with_filter(arr, f):
+    ''' Convolve a 2D array with a filter, using multiplication in the Fourier
+    space.
+
+    Parameters
+    ==========
+    arr : ndarray, shape (ni, nj)
+        Data values.
+    f : ndarray, shape (ni, nj)
+        Filter values, to be convolved with the data.
+
+    Returns
+    =======
+    conv : ndarray, shape (ni, nj)
+        The convolution of arr with f.
+    '''
+    conv = np.fft.ifft2(
+        np.fft.fft2(arr) * \
+        np.fft.fft2(f)
+        )
+    conv = roll_2d(conv.real)
+    return conv
+
+def unsharp_mask(arr, radius=10, strength=1):
+    ''' Apply unsharp masking to an array.
+
+    This filters-out low spatial frequencies from an image. A blurred version
+    of the image is first computed using a gaussian filter of standard
+    deviation `radius`. This blurred image is then scaled by the factor
+    `strength`, and removed from the original image.
+
+    Parameters
+    ==========
+    arr : array, 2D
+        Data values.
+    radius : float (default: 10)
+        Radius of the gaussian filter.
+    strength : float (default: 1)
+        Unsharp strength.
+
+    Returns
+    =======
+    filtered_arr : array, 2D
+        Filtered data values.
+    '''
+    ni, nj = arr.shape
+    smooth_arr = convolve_with_filter(arr, gauss2d(nj, ni, radius, radius))
+    filtered_arr = arr - smooth_arr * strength
+    return filtered_arr
